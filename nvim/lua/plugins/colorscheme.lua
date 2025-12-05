856169afa305153abd79bed7b9fe11ec12372c11
BiffTrain <windows95pc2@gmail.com>
2025-12-05 10:25:03 -0800

    lazyvim additions and theme

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+    "folke/tokyonight.nvim",
+    opts = {
+        transparent = true,
+        styles = {
+            -- This ensures components like the sidebar and float windows are also transparent
+            sidebars = "transparent",
+            floats = "transparent",
+        },
+    },
+}
